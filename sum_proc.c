b7fcfc43e5d13c142f5189a00b86b24c616b72e4
JeanVanneste <jean.vanneste@student.vinci.be>
2019-03-27 20:26:18 +0100

    Adding file reading

--- sum_proc.c
+++ sum_proc.c
@@ -6,43 +6,49 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-void readFile(char* filePath, int array[], int arraySize);
+#define ARRAY_SIZE 20
+
+void readArray(int array[], int arraySize);
 
 int main(){
-	char* filePath = "data";
-	const int arraySize = 100;
-	int array[arraySize];
+    char* filePath = "./data";
+    const int arraySize = ARRAY_SIZE;
+    int array[ARRAY_SIZE];
 
-	readFile(filePath, array, arraySize);
+    // Read file
+    FILE* file = fopen(filePath, "r");
+    if (file == NULL){ return 1; }
+    else
+    {
+        for (int i = 0; i<ARRAY_SIZE; i++)
+        {
+            fscanf(file, "%d", &array[i]);
+        }
+    }
+    readArray(array, arraySize);
+    
+    pid_t pid = fork();
 
-	pid_t pid = fork();
+    if (pid < 0){ printf("Error"); }
+    else if (pid == 0)
+    {
+        printf("Fils\n");
+        execlp("/bin/echo", "echo", "Hello world!", NULL);
+    }
+    else
+    {
+        printf("Parents\n");
+        wait(NULL);
 
-	if (pid < 0){ printf("Error"); }
-	else if (pid == 0)
-	{
-		printf("Fils\n");
-		execlp("/bin/echo", "echo", "Hello world!", NULL);
-	}
-	else
-	{
-		printf("Parents\n");
-		wait(NULL);
-
-		printf("Mon fils a fini\n");
-	}
-
-	return 0;
+        printf("Mon fils a fini\n");
+    }
+    return 0;
 }
 
-void readFile(char* filePath, int array[], int arraySize)
+void readArray(int array[], int arraySize)
 {
-	for (int i = 0; i<arraySize; i++)
-	{
-		array[i] = NULL;
-	}
-	FILE* file = fopen(filePath, "r");
-	char buffer[20];
-
-
-	fclose(file);
+    for (int i = 0; i<arraySize; i++)
+    {
+        printf("%d\n", array[i]);
+    }
 }
