70bd815d6857c21c8dee144046895a7e436fbc67
Diab0lix <info@diabolix.be>
2019-03-28 15:15:44 +0100

    Sum integers >10 and <10 in two separate processes and pass the results using pipes

--- sum_proc.c
+++ sum_proc.c
@@ -3,12 +3,16 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <time.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
 #define ARRAY_SIZE 20
 
 void readArray(int array[], int arraySize);
+
+int p[2];
+char buffer[6];
 
 int main(){
     char* filePath = "./data";
@@ -25,19 +29,54 @@ int main(){
             fscanf(file, "%d", &array[i]);
         }
     }
-    readArray(array, arraySize);
-    
+//    readArray(array, arraySize);
+
+    pipe (p);
     pid_t pid = fork();
 
     if (pid < 0){ printf("Error"); }
     else if (pid == 0)
     {
-        printf("Fils\n");
-        execlp("/bin/echo", "echo", "Hello world!", NULL);
+        int recv[4];
+
+        close (p[1]);
+        read (p[0], &recv[0], sizeof(recv[0]));
+        close (p[0]);
+
+        //buffer[5] = '\0';
+
+        int child_result = 0;
+        for (int i = 0; i<arraySize; i++)
+        {
+            if (i>10)
+            {
+                child_result+=array[i];
+            }
+        }
+        printf("child recv: %d\n", recv[0]);
+
+        int total_sum = child_result + recv[0];
+        printf("child result: %d\n", child_result);
+        printf("total sum: %d\n", total_sum);
     }
     else
     {
-        printf("Parents\n");
+        close (p[0]);
+
+
+        int parent_result = 0;
+        for (int i = 0; i<arraySize; i++)
+        {
+            if (i<10)
+            {
+                parent_result+=array[i];
+            }
+        }
+
+        write (p[1], &parent_result, sizeof(parent_result));
+        close (p[1]);
+
+        printf("parent result: %d\n", parent_result);
         wait(NULL);
 
         printf("Mon fils a fini\n");
@@ -52,3 +91,4 @@ void readArray(int array[], int arraySize)
         printf("%d\n", array[i]);
     }
 }
+
